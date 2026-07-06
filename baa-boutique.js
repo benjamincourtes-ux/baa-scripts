e443ba805386d6d133be862f760711914dbb8532
benjamincourtes-ux <benjamin.courtes@gmail.com>
2026-07-06 10:06:21 +0200

    Update baa-boutique.js

--- baa-boutique.js
+++ baa-boutique.js
@@ -1,5 +1,38 @@
 // BAA Boutique — Gestion boutique VDI
 
+var BAA_PRODUITS_MIHI = {
+  "make-up": { label:"💄 Make-Up", produits:[
+    {ref:"051201",nom:"Fond de teint léger Light Ivory",prix:32.90},{ref:"051202",nom:"Fond de teint léger Medium Beige",prix:32.90},{ref:"051203",nom:"Fond de teint léger Honey",prix:32.90},{ref:"051211",nom:"Fond de teint léger Sand",prix:32.90},{ref:"051204",nom:"Fond de teint matifiant Natural",prix:34.90},{ref:"051205",nom:"Fond de teint matifiant Golden Beige",prix:34.90},{ref:"051206",nom:"Fond de teint matifiant Caramel",prix:34.90},{ref:"051207",nom:"Fond de teint matifiant Mocca",prix:34.90},{ref:"052001",nom:"Palette de contour du visage",prix:38.90},{ref:"052002",nom:"Highlighter poudre",prix:28.90},{ref:"052003",nom:"Blush poudre",prix:26.90},{ref:"052004",nom:"Poudre fixante translucide",prix:24.90},{ref:"053001",nom:"Mascara volume & longueur",prix:22.90},{ref:"053002",nom:"Crayon yeux noir",prix:14.90},{ref:"053003",nom:"Fard à paupières palette",prix:34.90},{ref:"054001",nom:"Rouge à lèvres mat",prix:18.90},{ref:"054002",nom:"Gloss hydratant",prix:16.90},{ref:"054003",nom:"Crayon lèvres",prix:14.90},{ref:"055001",nom:"Pinceau fond de teint",prix:16.90},{ref:"055002",nom:"Set pinceaux maquillage",prix:38.90}
+  ]},
+  "visage": { label:"✨ Visage", produits:[
+    {ref:"010401",nom:"Clean Gel nettoyant hydratant",prix:24.90},{ref:"010402",nom:"Clean Eau micellaire hydratante",prix:22.90},{ref:"010404",nom:"Clean Mousse nettoyante douce",prix:26.90},{ref:"010405",nom:"Clean Tonique apaisant",prix:20.90},{ref:"010408",nom:"Clean Exfoliant crémeux",prix:28.90},{ref:"010409",nom:"Clean Démaquillant biphasé",prix:22.90},{ref:"010210",nom:"Retinol Plant Crème de jour",prix:42.90},{ref:"010211",nom:"Retinol Plant Masque-crème de nuit",prix:44.90},{ref:"010212",nom:"Retinol Plant Sérum",prix:46.90},{ref:"010213",nom:"Retinol Plant Crème yeux",prix:38.90},{ref:"010613",nom:"Hyaluron Pro Crème de jour SPF15",prix:42.90},{ref:"010614",nom:"Hyaluron Pro Sérum ultra-hydratant",prix:48.90},{ref:"010702",nom:"Mucin Crème de jour parfaite",prix:44.90},{ref:"010703",nom:"Mucin Crème de nuit intensive",prix:46.90},{ref:"010801",nom:"Tripeptydes Crème de jour relaxante",prix:52.90},{ref:"010803",nom:"Tripeptydes Crème de nuit raffermissante",prix:54.90},{ref:"010810",nom:"Tripeptydes Booster botox",prix:58.90},{ref:"011200",nom:"CBD Line Crème de jour régénérante",prix:46.90},{ref:"011600",nom:"Combi Skin Crème jour prébiotiques",prix:42.90},{ref:"011501",nom:"Special Care Crème jour SPF30",prix:44.90},{ref:"011800",nom:"Vitamin C Crème jour et nuit",prix:44.90},{ref:"011801",nom:"Vitamin C Sérum",prix:52.90},{ref:"011901",nom:"Skin Balance Crème peaux grasses",prix:38.90},{ref:"011902",nom:"Skin Balance Crème peaux sèches",prix:38.90},{ref:"012001",nom:"ExoLifting Crème jour et nuit",prix:68.90},{ref:"012003",nom:"ExoLifting Sérum aux spicules",prix:72.90}
+  ]},
+  "corps": { label:"🧴 Corps", produits:[
+    {ref:"020101",nom:"Lait corps hydratant",prix:28.90},{ref:"020102",nom:"Beurre corps nourrissant",prix:32.90},{ref:"020103",nom:"Gel douche doux",prix:18.90},{ref:"020104",nom:"Gommage corps énergisant",prix:30.90},{ref:"020105",nom:"Huile corps sèche",prix:34.90},{ref:"020106",nom:"Derma Help Crème adoucissante pieds",prix:22.90},{ref:"020107",nom:"Crème anti-cellulite",prix:38.90},{ref:"020108",nom:"Sérum raffermissant corps",prix:42.90},{ref:"020109",nom:"Crème mains nourrissante",prix:18.90}
+  ]},
+  "cheveux": { label:"💇 Cheveux", produits:[
+    {ref:"030101",nom:"Hair Help Shampooing anti-chute",prix:26.90},{ref:"030102",nom:"Hair Help Après-shampooing",prix:26.90},{ref:"030103",nom:"Hair Help Huile anti-chute",prix:34.90},{ref:"030104",nom:"Hair Help Baume anti-chute",prix:30.90},{ref:"030105",nom:"Hair Help Lotion antipelliculaire",prix:28.90},{ref:"030106",nom:"Hair Help Shampooing antipelliculaire",prix:26.90},{ref:"030201",nom:"Sebum Balance Shampooing cheveux gras",prix:24.90},{ref:"030202",nom:"Sebum Balance Après-shampooing",prix:24.90},{ref:"030301",nom:"Cannabis Intensive Huile pointes",prix:32.90},{ref:"030401",nom:"Curls Line Shampooing bouclés",prix:26.90},{ref:"030402",nom:"Curls Line Après-shampooing bouclés",prix:26.90},{ref:"030501",nom:"Bamboo Charcoal Shampooing détox",prix:26.90},{ref:"030502",nom:"Bamboo Charcoal Masque détox",prix:32.90},{ref:"030601",nom:"Vitamins Hair Formula",prix:38.90}
+  ]},
+  "poids": { label:"⚖️ Contrôle du poids", produits:[
+    {ref:"040101",nom:"Slimming Shake chocolat",prix:42.90},{ref:"040102",nom:"Slimming Shake vanille",prix:42.90},{ref:"040103",nom:"Brûleur de graisses",prix:38.90},{ref:"040104",nom:"Détox thé minceur",prix:28.90},{ref:"040105",nom:"Collagène beauté & minceur",prix:44.90}
+  ]},
+  "hommes": { label:"👨 Hommes", produits:[
+    {ref:"060101",nom:"Gel nettoyant visage homme",prix:22.90},{ref:"060102",nom:"Crème hydratante homme",prix:32.90},{ref:"060103",nom:"After-shave apaisant",prix:26.90},{ref:"060104",nom:"Gel douche homme",prix:18.90},{ref:"060105",nom:"Shampooing homme anti-chute",prix:24.90}
+  ]},
+  "enfants": { label:"👶 Enfants", produits:[
+    {ref:"070101",nom:"Gel douche enfant doux",prix:16.90},{ref:"070102",nom:"Shampooing enfant sans larmes",prix:16.90},{ref:"070103",nom:"Crème corps enfant",prix:18.90},{ref:"070104",nom:"Crème change bébé",prix:20.90}
+  ]},
+  "home": { label:"🏠 Home", produits:[
+    {ref:"080101",nom:"Lessive liquide naturelle",prix:24.90},{ref:"080102",nom:"Liquide vaisselle naturel",prix:14.90},{ref:"080103",nom:"Nettoyant multi-surfaces",prix:16.90},{ref:"080104",nom:"Adoucissant linge",prix:18.90}
+  ]},
+  "parfums": { label:"🌸 Parfums", produits:[
+    {ref:"090101",nom:"Eau de parfum Femme 1",prix:52.90},{ref:"090102",nom:"Eau de parfum Femme 2",prix:52.90},{ref:"090103",nom:"Eau de parfum Homme",prix:52.90},{ref:"090104",nom:"Brume parfumée corps",prix:28.90}
+  ]},
+  "sante": { label:"💊 Santé", produits:[
+    {ref:"100101",nom:"Vitamines & minéraux complexe",prix:38.90},{ref:"100102",nom:"Omega 3",prix:32.90},{ref:"100103",nom:"Probiotiques",prix:36.90},{ref:"100104",nom:"Collagène marin",prix:44.90},{ref:"022100",nom:"Oral Care Dentifrice blanchissant charbon",prix:16.90},{ref:"022101",nom:"Oral Care Dentifrice anti-caries",prix:14.90}
+  ]}
+};
+
 function openGestionBoutique() {
   if (document.getElementById("baa-boutique-panel")) return;
 
